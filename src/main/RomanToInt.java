7441f550a319c0b95b7126b45c23d3abbdb1be8e
thsc47 <thsc47@gmail.com>
2023-12-17 11:54:09 -0300

    feat: add `RomanToInt` challenge code

---
+++ src/main/RomanToInt.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInt {
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                answer -= m.get(s.charAt(i));
+            } else {
+                answer += m.get(s.charAt(i));
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("MCMXCIV"));
+    }
+}
