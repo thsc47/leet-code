1fa430e2bb4640971f476167f6dfd266ccc6cd68
thsc47 <thsc47@gmail.com>
2024-01-12 14:13:01 -0300

    feat: add `ReverseLinkedList` challenge code

---
+++ src/main/ReverseLinkedList.java
@@ -0,0 +1,19 @@
+package main;
+
+public class ReverseLinkedList {
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            ListNode current = head;
+
+
+            while(current != null) {
+                ListNode next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            return prev;
+        }
+    }
+}
